50b673cbb2ed044b5246df2fa4fe7bd97ffa4a04
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-01-20 18:16:44 -0500

    Working on Stretch. Form created.

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
-import GitHubUserCard from "./components/GitHubUserCard";
-import FollowersList from "./components/FollowersList";
-import ProjectsList from "./components/ProjectsList";
+import GitHubUserCard from "./Components/GitHubUserCard";
+import FollowersList from "./Components/FollowersList";
 import { Container, Typography } from "@material-ui/core";
+import axios from "axios";
+import "./App.scss";
+import logo from "./assets/githublogo.png";
 
 class App extends React.Component {
 	state = {
@@ -10,9 +12,9 @@ class App extends React.Component {
 	};
 
 	componentDidMount() {
-		fetch(`https://api.github.com/users/${this.state.userToFetch}`)
-			.then(res => res.json())
-			.then(user => this.setState({ ...this.state, user: user }))
+		axios
+			.get(`https://api.github.com/users/${this.state.userToFetch}`)
+			.then(user => this.setState({ ...this.state, user: user.data }))
 			.catch(err => console.log("Error in componentDidMount: ", err));
 	}
 
@@ -38,20 +40,7 @@ class App extends React.Component {
 			)
 			.catch(err => console.log("Error in fetchFollowers: ", err));
 
-	fetchProjects = userToFetch => {
-		fetch(`https://api.github.com/users/${userToFetch}/repos`)
-			.then(res => res.json())
-			.then(projects =>
-				this.setState({
-					...this.state,
-					followers: undefined,
-					projects: projects
-				})
-			)
-			.catch(err => console.log("Error in fetchProjects: ", err));
-	};
-
-	handleUserButton = userToFetch =>
+	handleUserChange = userToFetch =>
 		this.setState({ ...this.state, userToFetch: userToFetch });
 
 	handleSearchInput = event =>
@@ -76,10 +65,14 @@ class App extends React.Component {
 	};
 
 	render() {
-		const { user, followers, projects } = this.state;
+		const { user, followers } = this.state;
 		console.log(this.state);
 		return user ? (
 			<Container maxWidth='xs'>
+				<header>
+					<img src={logo} />
+					<h1>User Page!</h1>
+				</header>
 				<Typography component='div'>
 					<GitHubUserCard
 						user={user}
@@ -90,11 +83,10 @@ class App extends React.Component {
 						followers={followers}
 						searchFollowers={this.searchFollowers}
 						clearForm={this.clearForm}
-						handleUserButton={this.handleUserButton}
+						handleUserChange={this.handleUserChange}
 						handleSearchInput={this.handleSearchInput}
 						searchTerm={this.state.searchTerm || ""}
 					/>
-					<ProjectsList projects={projects} />
 				</Typography>
 			</Container>
 		) : (
--- app/src/Components/GitHubUserCard.js
+++ app/src/Components/GitHubUserCard.js
@@ -1,52 +1,52 @@
 import React from "react";
 import {
-  Typography,
-  Card,
-  CardActions,
-  CardActionArea,
-  CardMedia,
-  CardContent,
-  Button
+	Typography,
+	Card,
+	CardActions,
+	CardActionArea,
+	CardMedia,
+	CardContent,
+	Button
 } from "@material-ui/core";
 
 class GitHubUserCard extends React.Component {
-  render() {
-    return (
-      <Card>
-        <CardActionArea>
-          <CardMedia
-            style={{ height: 320 }}
-            image={this.props.user.avatar_url}
-            title={this.props.user.name}
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              {this.props.user.name}
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              {this.props.user.bio}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button
-            size="small"
-            color="primary"
-            onClick={() => this.props.fetchFollowers(this.props.user.login)}
-          >
-            Show Followers
-          </Button>
-          <Button
-            size="small"
-            color="primary"
-            onClick={() => this.props.fetchProjects(this.props.user.login)}
-          >
-            Show Projects
-          </Button>
-        </CardActions>
-      </Card>
-    );
-  }
+	render() {
+		return (
+			<Card>
+				<CardActionArea>
+					<CardMedia
+						style={{ height: 320 }}
+						image={this.props.user.avatar_url}
+						title={this.props.user.name}
+					/>
+					<CardContent>
+						<Typography gutterBottom variant='h5' component='h2'>
+							{this.props.user.name}
+						</Typography>
+						<Typography variant='body2' color='textSecondary' component='p'>
+							GitHub Handle: {this.props.user.login}
+						</Typography>
+						<Typography variant='body2' color='textSecondary' component='p'>
+							{this.props.user.bio}
+						</Typography>
+						<Typography variant='body2' color='textSecondary' component='p'>
+							{this.props.user.location}
+						</Typography>
+					</CardContent>
+				</CardActionArea>
+				<CardActions>
+					<Button
+						className='btn'
+						size='small'
+						color='secondary'
+						onClick={() => this.props.fetchFollowers(this.props.user.login)}
+					>
+						Show Followers
+					</Button>
+				</CardActions>
+			</Card>
+		);
+	}
 }
 
 export default GitHubUserCard;
