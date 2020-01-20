f95430267278c6d1e118ca3e5389b5014cb8ca21
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-01-20 17:42:48 -0500

    MVP met

--- app/src/App.js
+++ app/src/App.js
@@ -1,37 +1,104 @@
 import React from "react";
-import "./App.scss";
-import logo from "./assets/githublogo.png";
-import axios from "axios";
-import UserCard from "./Components/UserCard";
+import GitHubUserCard from "./components/GitHubUserCard";
+import FollowersList from "./components/FollowersList";
+import ProjectsList from "./components/ProjectsList";
+import { Container, Typography } from "@material-ui/core";
 
 class App extends React.Component {
 	state = {
-		users: []
+		userToFetch: "PrinceD96"
 	};
 
 	componentDidMount() {
-		axios
-			.get("https://api.github.com/users/PrinceD96")
-			.then(res => {
-				console.log("res:", res);
-				this.setState({ ...this.state, users: res.data });
-			})
-			.catch(error => {
-				console.log("Something went wrong =>", error);
-			});
+		fetch(`https://api.github.com/users/${this.state.userToFetch}`)
+			.then(res => res.json())
+			.then(user => this.setState({ ...this.state, user: user }))
+			.catch(err => console.log("Error in componentDidMount: ", err));
 	}
 
+	componentDidUpdate(prevProps, prevState) {
+		if (prevState.userToFetch !== this.state.userToFetch) {
+			fetch(`https://api.github.com/users/${this.state.userToFetch}`)
+				.then(res => res.json())
+				.then(user => this.setState({ ...this.state, user: user }))
+				.then(() => this.fetchFollowers(this.state.userToFetch))
+				.catch(err => console.log("Error in componentDidUpdate: ", err));
+		}
+	}
+
+	fetchFollowers = userToFetch =>
+		fetch(`https://api.github.com/users/${userToFetch}/followers`)
+			.then(res => res.json())
+			.then(followers =>
+				this.setState({
+					...this.state,
+					projects: undefined,
+					followers: followers
+				})
+			)
+			.catch(err => console.log("Error in fetchFollowers: ", err));
+
+	fetchProjects = userToFetch => {
+		fetch(`https://api.github.com/users/${userToFetch}/repos`)
+			.then(res => res.json())
+			.then(projects =>
+				this.setState({
+					...this.state,
+					followers: undefined,
+					projects: projects
+				})
+			)
+			.catch(err => console.log("Error in fetchProjects: ", err));
+	};
+
+	handleUserButton = userToFetch =>
+		this.setState({ ...this.state, userToFetch: userToFetch });
+
+	handleSearchInput = event =>
+		this.setState({ ...this.state, searchTerm: event.target.value });
+
+	searchFollowers = event => {
+		event.preventDefault();
+		this.setState({
+			...this.state,
+			followers: this.state.followers.filter(follower =>
+				follower.login
+					.toLowerCase()
+					.includes(this.state.searchTerm.toLowerCase())
+			)
+		});
+	};
+
+	clearForm = event => {
+		event.preventDefault();
+		this.fetchFollowers(this.state.userToFetch);
+		this.setState({ ...this.state, searchTerm: "" });
+	};
+
 	render() {
-		return (
-			<div className='App'>
-				<header>
-					<img src={logo} alt='logo' />
-					<h1>User Page!</h1>
-				</header>
-				<div className='card-list'>
-					<UserCard state={this.state} />
-				</div>
-			</div>
+		const { user, followers, projects } = this.state;
+		console.log(this.state);
+		return user ? (
+			<Container maxWidth='xs'>
+				<Typography component='div'>
+					<GitHubUserCard
+						user={user}
+						fetchFollowers={this.fetchFollowers}
+						fetchProjects={this.fetchProjects}
+					/>
+					<FollowersList
+						followers={followers}
+						searchFollowers={this.searchFollowers}
+						clearForm={this.clearForm}
+						handleUserButton={this.handleUserButton}
+						handleSearchInput={this.handleSearchInput}
+						searchTerm={this.state.searchTerm || ""}
+					/>
+					<ProjectsList projects={projects} />
+				</Typography>
+			</Container>
+		) : (
+			<div></div>
 		);
 	}
 }
